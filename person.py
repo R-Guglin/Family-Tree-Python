51a029c6fc2a743e9fcd04d006d966308cffc1b6
R-Guglin <147883918+R-Guglin@users.noreply.github.com>
2023-11-17 12:02:15 -0800

    Update person.py

--- person.py
+++ person.py
@@ -114,36 +114,6 @@ class Person:
                 out += "   " + child.name[0] + " " + child.name[2]+"\n"
         return out + "\n"
 
-    ''' Class invariants:
-    - An object of the class Person is initialized as the root of an empty tree.
-    - name is a list [First, Middle, Last]; birthdate is [Day, Month, Year] because that's how most of the world does it.
-    - gender is either "M" or "F" OR a list [Subject, Object, Possessive] if the person uses different pronouns.
-    - bio is an optional JSON with cool stuff like a biography and photo(s).
-    ... there are some functions for adding bio stuff to a Person, all stored in bio.py so that their heinous
-        bulk doesn't clutter up and desecrate this class module.
-    - gen is the number of generations between this object and the root of the tree it's in; initialized to 0.
-    - If initialized with a parent and/or children, the constructor changes generations as needed, using updateGen().
-
-    - since all the parameters of __init__ are technically optional, we have the following default Person:
-        Name: Empty string
-        Pronouns: they/them/theirs
-        Age: 2023 years old
-        No parent
-        No children
-        No bio
-
-    - There is another constructor, class method fromFile(), which takes in a JSON file and creates one or many 
-      People as instructed... which is helpful when we want to store a family tree over time--be warned, though, that 
-      your JSONs can get very long, which is why there's also
-    - a toFile() method, which creates a JSON file describing THE TREE ROOTED AT THIS PERSON,
-      not the whole family tree! The user is responsible for saving trees properly.
-      However, you can put JSONs in JSONs (in JSONs, in JSONs...) -- so I can save Mom.json and then 
-      include it later on in Grandma.json. Grandma's fromFile() constructor will then consider that Mom is now 
-      trapped in that awful JSON file, and it will build a nice Mom Person out of Mom.json, and add Mom to Grandma's
-      children.
-    '''
-         
-
 # Family tree toolkit
 
 def isDescendant(A, B):
